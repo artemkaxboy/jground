4fc4b6770f00dee894d910ba237d7cbd9d0064f8
Artem Kolin <artemkaxboy@gmail.com>
2023-03-30 00:55:07 +0400

    Leet 983, Accepted
    Runtime 276 ms   Beats 5.45%
    Memory 38.8 MB   Beats 7.27%

--- src/main/java/com/artemkaxboy/kground/Leet983.kt
+++ src/main/java/com/artemkaxboy/kground/Leet983.kt
@@ -1,5 +1,6 @@
 package com.artemkaxboy.kground
 
+import java.util.TreeMap
 import kotlin.system.measureTimeMillis
 
 /**
@@ -18,117 +19,57 @@ import kotlin.system.measureTimeMillis
  */
 class Leet983 {
 
-    val D_PASS = 0
-    val W_PASS = 1
-    val M_PASS = 2
-    val PASS_DAYS = intArrayOf(1, 7, 30)
+    private val map = TreeMap<Int, Int>()
 
     fun mincostTickets(days: IntArray, costs: IntArray): Int {
 
+        (listOf(0) + days.asList()).zipWithNext().forEach { (prev, it) -> calcFor(prev, it, costs) }
+        println(map)
+        return findLowestValueAfterKey(days.last())
+    }
+
+    private fun calcFor(prevDay: Int, day: Int, costs: IntArray) {
+        val offset = findLowestValueAfterKey(prevDay)
+
+        map[day + 1] = minOfNullableMax(map[day + 1], offset + costs[0])
+        map[day + 7] = minOfNullableMax(map[day + 7], offset + costs[1])
+        map[day + 30] = minOfNullableMax(map[day + 30], offset + costs[2])
+    }
+
+    private fun minOfNullableMax(a: Int?, b: Int) = minOf(a ?: Int.MAX_VALUE, b)
+
+    private fun findLowestValueAfterKey(keyGt: Int): Int {
+        return minOrNull(map.filterKeys { key -> key > keyGt }.values) ?: 0
+    }
+
+    private fun minOrNull(iterable: Iterable<Int>): Int? {
+        val iterator = iterable.iterator()
+        if (!iterator.hasNext()) return null
+
         var min = Int.MAX_VALUE
-        val m = calcFor(M_PASS, days, costs, 0, min)
-//        println(m)
-        val w = calcFor(W_PASS, days, costs, 0, min)
-//        println(w)
-        val d = calcFor(D_PASS, days, costs, 0, min)
-//        println(d)
-
-        min = minOf(min, m)
-        min = minOf(min, w)
-        min = minOf(min, d)
+        while (iterator.hasNext()) {
+            val candidate = iterator.next()
+            if (candidate < min) min = candidate
+        }
         return min
     }
 
-    fun calcFor(pass: Int, days: IntArray, costs: IntArray, acc: Int, knownMin: Int): Int {
-//        println("pass: $pass, acc: $acc, knownMin: $knownMin")
-        if (acc >= knownMin) return acc
-        val next = days.firstOrNull()?.let { it + PASS_DAYS[pass] } ?: return 0
-        val remain = days.dropWhile { it < next }.toIntArray()
-        val newAcc = acc + costs[pass]
-        if (remain.isEmpty()) return newAcc
-        if (newAcc >= knownMin) return newAcc
-
-        var min = knownMin
-        (2 downTo 0).forEach { min = minOf(min, calcFor(it, remain, costs, newAcc, min)) }
-        return min
-    }
 }
 
 fun main() {
     measureTimeMillis {
-//    println(Leet983().mincostTickets(intArrayOf(1, 4, 6, 7, 8, 20), intArrayOf(2, 7, 15))) // 11
-//    println(Leet983().mincostTickets(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31), intArrayOf(2, 7, 15))) // 17
-//    println(
-//        Leet983().mincostTickets(
-//            intArrayOf(1, 4, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 27, 28),
-//            intArrayOf(3, 13, 45)
-//        )
-//    ) // 44
+//        println(Leet983().mincostTickets(intArrayOf(1, 4, 6, 7, 8, 20), intArrayOf(2, 7, 15))) // 11
+        println(Leet983().mincostTickets(intArrayOf(1, 4, 6, 7, 8, 20), intArrayOf(7, 2, 15))) // 6
+//        println(Leet983().mincostTickets(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31), intArrayOf(2, 7, 15))) // 17
+//        println(
+//            Leet983().mincostTickets(
+//                intArrayOf(1, 4, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 27, 28),
+//                intArrayOf(3, 13, 45)
+//            )
+//        ) // 44
 //        println(Leet983().mincostTickets(intArrayOf(1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 24, 25, 27, 28, 29, 30, 31, 34, 37, 38, 39, 41, 43, 44, 45, 47, 48, 49, 54, 57, 60, 62, 63, 66, 69, 70, 72, 74, 76, 78, 80, 81, 82, 83, 84, 85, 88, 89, 91, 93, 94, 97, 99), intArrayOf(9, 38, 134))) // ??
-        println(
-            Leet983().mincostTickets(
-                intArrayOf(
-                    2,
-                    3,
-                    5,
-                    6,
-                    7,
-                    8,
-                    9,
-                    10,
-                    11,
-                    17,
-                    18,
-                    19,
-                    23,
-                    26,
-                    27,
-                    29,
-                    31,
-                    32,
-                    33,
-                    34,
-                    35,
-                    36,
-                    38,
-                    39,
-                    40,
-                    41,
-                    42,
-                    43,
-                    44,
-                    45,
-                    47,
-                    51,
-                    54,
-                    55,
-                    57,
-                    58,
-                    64,
-                    65,
-                    67,
-                    68,
-                    72,
-                    73,
-                    74,
-                    75,
-                    77,
-                    78,
-                    81,
-                    86,
-                    87,
-                    88,
-                    89,
-                    91,
-                    93,
-                    94,
-                    95,
-                    96,
-                    98,
-                    99
-                ), intArrayOf(5, 24, 85)
-            )
-        ) // ??
+//        println(Leet983().mincostTickets(intArrayOf(2, 3, 5, 6, 7, 8, 9, 10, 11, 17, 18, 19, 23, 26, 27, 29, 31, 32, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 47, 51, 54, 55, 57, 58, 64, 65, 67, 68, 72, 73, 74, 75, 77, 78, 81, 86, 87, 88, 89, 91, 93, 94, 95, 96, 98, 99), intArrayOf(5, 24, 85))) // ??
+
 
     }.also { println("Time: ${it}ms") }
 }
